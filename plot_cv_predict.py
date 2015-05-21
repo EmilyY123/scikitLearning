cd629feff088d5ba6b48c539298fd1c96b17daa9
leonfliu@gmail.com <leonfliu@gmail.com>
2015-05-20 20:28:37 -0500

    scikit learning tutorial

--- plot_cv_predict.py
+++ plot_cv_predict.py
@@ -1,5 +1,7 @@
 from sklearn import datasets
-from sklearn.cross_validation import cross_val_predict
+from sklearn.cross_validation import cross_val_predict 
+# cross_val_predict(estimator,X,y,cv,n_jobs) default for cv is 3; n_jobs: optional, # of CPU use to do the computation, -1 = all CPUs  
+# cross_val_predict return predicted values (array)
 from sklearn import linear_model
 import matplotlib.pyplot as plt 
 import pylab
@@ -8,6 +10,8 @@ lr = linear_model.LinearRegression()
 boston = datasets.load_boston()
 y = boston.target 
 
+# cross_val_predict returns an array of the same size as `y` where each entry
+# is a prediction obtained by cross validated:
 predicted = cross_val_predict(lr, boston.data, y, cv = 10)
 
 fig, ax = plt.subplots()
@@ -15,4 +19,4 @@ ax.scatter(y,predicted)
 ax.plot([y.min(), y.max()], [y.min(), y.max()], 'k--', lw = 4)
 ax.set_xlabel('Measured')
 ax.set_ylabel('Predicted')
-pylab.show()
+pylab.show()  # use pylab.show() in order to show figure not fig.show() here 
